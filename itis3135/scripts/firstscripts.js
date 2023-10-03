8a3b4d05a2f9ffafeb3e2ff9432d1e84bebb1f81
kspark12 <kspark12@uncc.edu>
2023-10-02 21:26:23 -0400

    appended my header and footer

---
+++ itis3135/scripts/firstscripts.js
@@ -0,0 +1,43 @@
+function greetUser() {
+    const userName = document.getElementById("userName").value;
+    const userMood = document.getElementById("userMood").value;
+    const userNumber = Math.abs(Math.round(parseFloat(document.getElementById("userNumber").value)));
+
+    const greeting = document.getElementById("greeting");
+    greeting.innerHTML = `The MyCompany welcomes you, ${userName}!<br>We're glad you are doing ${userMood}!<br>`;
+    
+    // Determine and display the polygon name
+    const polygonName = getPolygonName(userNumber);
+    alert(`The polygon with ${userNumber} sides is called a ${polygonName}`);
+}
+
+function getPolygonName(sides) {
+    const polygonNames = {
+        1: "Henagon",
+        2: "Digon",
+        3: "Trigon",
+        4: "Tetragon",
+        // Add more polygon names as needed
+    };
+    return polygonNames[sides] || "Unknown";
+}
+
+function provideInsult() {
+    // Function to provide a harmless insult
+    alert("You're so unique that you're a real rarity!");
+}
+
+function calculateTax() {
+    // Function to calculate tax (replace with your own calculation)
+    alert("Tax calculation: You owe $1000.");
+}
+
+function provideQuote() {
+    // Function to provide an inspirational quote
+    alert("Inspirational Quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.' - Winston Churchill");
+}
+
+function provideCompliment() {
+    // Function to provide a compliment
+    alert("You're amazing and capable of achieving anything you set your mind to!");
+}
