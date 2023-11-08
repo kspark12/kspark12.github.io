81486e98eed676069ee241cdaeb0320bab54bad6
kspark12 <kspark12@uncc.edu>
2023-11-08 18:59:05 -0500

    fizz buzz creation

---
+++ itis3135/scripts/fizzbuzz.js
@@ -0,0 +1,30 @@
+const fizzBuzzParagraph= document.getElementById("fizzbuzzcontent");
+
+//let text = createTextNode("FizzBuzz");
+// fizzBuzzParagraph.appendChild(text);
+
+
+function fizzBuzz(){
+    for (let i = 1; i <= 100; i++) {
+       let tempStr= "";
+
+       if(i%3==0){
+           tempStr+="Fizz";
+       }
+
+       if(i%5==0){
+           tempStr+="Buzz";
+       }
+
+       if(i%7==0){
+              tempStr+="BANG!";
+       }
+
+       if(tempStr==""){
+           tempStr+=i;
+       }
+       fizzBuzzParagraph.textContent+=tempStr + ", ";
+    }
+}
+
+fizzBuzz();
